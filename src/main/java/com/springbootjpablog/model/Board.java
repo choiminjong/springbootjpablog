00f2f4d06bbcea4c32c1bc62c3e22d1e23c517e8
alswhd20 <alswhd20@gmail.com>
2022-03-11 20:09:22 +0900

    reply Entity 생성

--- src/main/java/com/springbootjpablog/model/Board.java
+++ src/main/java/com/springbootjpablog/model/Board.java
@@ -1,32 +1,39 @@
 package com.springbootjpablog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Board {
 
-    @Id     //Primary Key
+    @Id                                                 //Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 사용한다.
-    private Long id; //시퀀스
+    private Long id;                                    //시퀀스
 
     @Column(nullable = false, length = 100)
     private String title;
     
     @Lob //대용량 데이터 사용
-    private String content; //섬머노트 라이브러리 사용
+    private String content;      //섬머노트 라이브러리 사용
 
     @ColumnDefault("0")
-    private Long count; //조회수
+    private Long count;          //조회수
 
-    @ManyToOne  // Many=board , User=One
+    @ManyToOne                   // Many=board , User=One
     @JoinColumn(name="userId")   //board 테이블에 User 테이블을 참조할 수 있는 FOREIGN KEY 자동으로 생성된다.
-    private User user; //DB는 오브젝트를 저장할 수 없다.FK,자바는 오브젝트를 저장할 수 있다.
+    private Users users;         //DB는 오브젝트를 저장할 수 없다.FK,자바는 오브젝트를 저장할 수 있다.
 
-    @CreationTimestamp
+    @CreationTimestamp           // 저장시 시간이 자동 입력
     private Timestamp creatData;
-
 }
--- src/main/java/com/springbootjpablog/model/Reply.java
+++ src/main/java/com/springbootjpablog/model/Reply.java
@@ -1,4 +1,36 @@
 package com.springbootjpablog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Reply {
+
+    @Id                                                 //Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 사용한다.
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne                   // 하나의 게시글에 여러개의댓글을 가질수 있다.
+    @JoinColumn(name="boardId")
+    private Board board;
+
+    @ManyToOne                  // 하나의 유저는 여러개 댓글을 작성할 수 있따.
+    @JoinColumn(name="userId")
+    private Users users;
+
+    @CreationTimestamp           // 저장시 시간이 자동 입력
+    private Timestamp creatData;
 }
