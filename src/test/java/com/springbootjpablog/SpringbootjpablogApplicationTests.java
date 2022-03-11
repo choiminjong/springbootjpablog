3ee557439c77011dd7557e04f348e4ced1cea81f
민종 최 <dominic@nexon.co.kr>
2022-03-11 09:16:07 +0900

    Initial commit

---
+++ src/test/java/com/springbootjpablog/SpringbootjpablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjpablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
