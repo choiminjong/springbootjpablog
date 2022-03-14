ba9f7c9a62d83624b5d4fa7bb6cef26a07c3d2cc
민종 최 <dominic@nexon.co.kr>
2022-03-14 09:22:04 +0900

    select/update 실행방법

--- src/main/java/com/springbootjpablog/controller/DummyControllerTest.java
+++ src/main/java/com/springbootjpablog/controller/DummyControllerTest.java
@@ -4,9 +4,15 @@ import com.springbootjpablog.model.RoleType;
 import com.springbootjpablog.model.Users;
 import com.springbootjpablog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -15,6 +21,38 @@ public class DummyControllerTest {
 
     @Autowired //의존성 주입
     private UserRepository userRepository;
+
+    @PutMapping("/dummy/user/{id}")
+    public Users updateUser(@PathVariable Long id, @RequestBody Users requestUser){
+
+        System.out.println("id = " + id);
+        System.out.println("password = " + requestUser.getPassword());
+        System.out.println("email = " + requestUser.getEmail());
+
+        Users user = userRepository.findById(id).orElseThrow(()->{
+                return new IllegalStateException("수정에 실패했습니다.");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        userRepository.save(user);
+        return null;
+    }
+
+    //http://localhost:8080/dummy/user (요청)
+    @GetMapping("/dummy/user")
+    public List<Users> list(){
+        return userRepository.findAll();
+    }
+
+    //한페이지당 2건 데이터를 리턴받아 볼 수 있도록 설정
+    @GetMapping("/dummy/user/page")
+    public Page<Users> pageList(@PageableDefault(size=2,sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        Page<Users> users = userRepository.findAll(pageable);
+        return users;
+    }
+
 
     //{id} 주소로 파마미터를 전달 받을 수 있음.
     //http://localhost:8080/dummy/user/1 (요청)
