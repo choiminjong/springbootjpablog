b4197f0362aa125c73a45e684b10825e78103d55
민종 최 <dominic@nexon.co.kr>
2022-04-05 08:12:32 +0900

    로그인 및 회워가입 경로 변경

--- src/main/java/com/springbootjpablog/controller/UserController.java
+++ src/main/java/com/springbootjpablog/controller/UserController.java
@@ -1,24 +1,21 @@
 package com.springbootjpablog.controller;
 
-import com.springbootjpablog.model.Board;
 import com.springbootjpablog.model.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/user/account")
+    @GetMapping("/joinForm")
     public String account(Model model) {
         model.addAttribute("user", new Users());
-        return "user/account";
+        return "user/joinForm";
     }
 
-    @GetMapping("/user/login")
+    @GetMapping("/loginForm")
     public String login() {
-        return "user/login";
+        return "user/loginForm";
     }
 }
