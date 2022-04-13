8304251f8ed1b1f34c0b64423f2f98d3e535bcf9
민종 최 <dominic@nexon.co.kr>
2022-04-13 11:39:06 +0900

    카카오 로그인 엑세스 토큰 받기

--- src/main/java/com/springbootjpablog/controller/view/UserController.java
+++ src/main/java/com/springbootjpablog/controller/view/UserController.java
@@ -1,9 +1,17 @@
 package com.springbootjpablog.controller.view;
 
 import com.springbootjpablog.model.entity.Users;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -25,4 +33,51 @@ public class UserController {
         return "user/updateForm";
     }
 
+    @GetMapping("/auth/kakao")
+    public String kakaoLogin() {
+        return "redirect:https://kauth.kakao.com/oauth/authorize?client_id=b513d8919eb20fb7c475f7474c840680&redirect_uri=http://localhost:8080/auth/kakao/callback&response_type=code";
+    }
+
+    /*
+     @ResponseBody 어노테이션 HTTP요청 바디를 자바객체로 변환하고 자바객체를 다시 HTTP 응답 바디로 변환해준다.
+    */
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code){
+        /*
+        MIME=application/x-www-form-urlencoded;charset=utf-8
+        grant_type=authorization_code
+        client_id=b513d8919eb20fb7c475f7474c840680
+        redirect_uri=http://localhost:8080/auth/kakao/callback
+        code=code
+         */
+        
+        //POST 방식으로 key=value 데이터를 카카오 로그인 서버로 전달
+        RestTemplate rt = new RestTemplate();
+        
+        //HttpHeaders 오브젝트 생성
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        //HttpBody 오브젝트 생성
+        //params.add는 변수를 따로 설정해서 사용해야하는데 직관적으로 이해하기위해서 다이렉트로 설정
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type","authorization_code");
+        params.add("client_id","b513d8919eb20fb7c475f7474c840680");
+        params.add("redirect_uri","http://localhost:8080/auth/kakao/callback");
+        params.add("code",code);
+        
+        //HttpHeaders와 HttpBody 오브젝트를 하나의 오브젝트로 담기
+        HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
+                new HttpEntity<>(params,httpHeaders);
+
+        //Http 요청하기 -> Post 방식으로 요청하면 response 변수로 데이터를 응답 받음
+        ResponseEntity response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token", //어떤 곳으로 전달할건지
+                HttpMethod.POST,    //메소드 타입
+                kakaoTokenRequest,  // HttpEntity 객체가
+                String.class        //응답 데이터 타입
+        );
+
+        return "카카오 토큰 요청 : 토큰 요청 데이터  "+ response;
+    }
 }
--- src/main/java/com/springbootjpablog/security/config/SecurityConfig.java
+++ src/main/java/com/springbootjpablog/security/config/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration // 빈등록 (IoC관리)
 @EnableWebSecurity // 시큐리티 필터가 등록이 된다.
@@ -48,16 +53,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(principalDetailService).passwordEncoder(passwordEncoder());
     }
 
+    // CORS 허용 적용
+    //Cors 허용 정책 설정하는 Bean
+    //    * addAllowedOrigin() : 허용할 URL
+    //    * addAllowedHeader() : 허용할 Header
+    //    * addAllowedMethod() : 허용할 Http Method
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                //.cors().configurationSource(corsConfigurationSource()) //CorsConfigurationSource 를 cors 정책의 설정파일
+        //.and()
                 .authorizeRequests()
                 .antMatchers("/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
-
         .and()
                 .formLogin()
                 .loginPage("/auth/loginForm")
